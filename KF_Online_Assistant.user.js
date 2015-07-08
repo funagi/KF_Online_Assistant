b1df0273d65181bbd79a673f2011b57096ca26d2
miaolapd <miaolapd@163.com>
2015-07-08 15:26:29 +0800

    V3.9.0发布

--- KF_Online_Assistant.user.js
+++ KF_Online_Assistant.user.js
@@ -9,7 +9,7 @@
 // @include     http://*.2dgal.com/*
 // @include     http://9baka.com/*
 // @include     http://*.9baka.com/*
-// @version     3.9.0-dev
+// @version     3.9.0
 // @grant       none
 // @run-at      document-end
 // @license     MIT
